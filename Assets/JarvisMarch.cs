3ae16a8189824d67c274bce8cd8cac9b58490b91
Sebastian Lague <sebastianlague@Sebastians-Mac-Pro.local>
2017-06-15 19:12:29 +0200

    Added jarvis march implementation + speed testing

---
+++ Assets/JarvisMarch.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JarvisMarch : IHull {
+
+	public List<int> GetHullPoints(Vector2[] points) {
+		List<int> hullPoints = new List<int> ();
+
+		float leftmostX = float.MaxValue;
+		int firstHullPointIndex = 0;
+
+		// find leftmost point (guaranteed to be hull point)
+		for (int i = 0; i < points.Length; i++) {
+			if (points [i].x < leftmostX) {
+				leftmostX = points [i].x;
+				firstHullPointIndex = i;
+			}
+		}
+
+		int pointOnHullIndex = firstHullPointIndex;
+
+		while (true) {
+
+
+			hullPoints.Add (pointOnHullIndex);
+
+			int endpointIndex = 0;
+
+			for (int i = 1; i < points.Length; i++) {
+				if (endpointIndex == pointOnHullIndex || Geometry.SideOfLine(points[endpointIndex],points[pointOnHullIndex],points[i]) == -1) {
+					endpointIndex = i;
+				}
+			}
+
+			pointOnHullIndex = endpointIndex;
+
+			if (endpointIndex == firstHullPointIndex) {
+				break;
+			}
+		}
+
+		return hullPoints;
+	}
+
+}
--- Assets/Test.cs
+++ Assets/Test.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class Test : MonoBehaviour {
 
+	public enum Algorithm {QuickHull,JarvisMarch
+	};
+
+	public Algorithm algorithm;
+
 	public int numPoints = 50;
 	public float radius = 20;
 	public int seed;
+	public bool drawLines;
+	public bool logTime;
+	public int iterations;
+
 	Vector2[] points;
 	List<int> hullPointIndices;
 
@@ -17,9 +26,28 @@ public class Test : MonoBehaviour {
 			points [i] = Random.insideUnitCircle * radius;
 		}
 
-		IHull hull = new QuickHull ();
+		//IHull hull = new QuickHull ();
+		IHull hull = new JarvisMarch();
+
+		switch (algorithm) {
+		case Algorithm.JarvisMarch:
+			hull = new JarvisMarch ();
+			break;
+		case Algorithm.QuickHull:
+			hull = new QuickHull ();
+			break;
+		}
 		hullPointIndices = hull.GetHullPoints (points);
 
+		if (logTime) {
+			System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch ();
+			sw.Start ();
+			for (int i = 0; i < iterations; i++) {
+				hull.GetHullPoints (points);
+			}
+			sw.Stop ();
+			print ("time: " + sw.ElapsedMilliseconds + "  iterations: " + iterations);
+		}
 	}
 	
 
@@ -28,6 +56,13 @@ public class Test : MonoBehaviour {
 			for (int i = 0; i < points.Length; i++) {
 				Gizmos.color = (hullPointIndices.Contains(i))?Color.red:Color.white;
 				Gizmos.DrawSphere (points[i], .3f);
+			}
+
+			if (drawLines) {
+				Gizmos.color = Color.green;
+				for (int i = 0; i < hullPointIndices.Count; i++) {
+					Gizmos.DrawLine(points[hullPointIndices[i]],points[hullPointIndices[(i+1)%hullPointIndices.Count]]);
+				}
 			}
 		}
 	}
